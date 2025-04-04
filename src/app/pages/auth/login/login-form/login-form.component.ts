02fe2170e5ca81b6671c168de17516328b87c4fa
Denis López <denuxs@gmail.com>
2025-04-04 12:23:56 -0600

    refactor: login form and translate

---
+++ src/app/pages/auth/login/login-form/login-form.component.ts
@@ -0,0 +1,60 @@
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import { PasswordModule } from 'primeng/password';
+import { InputTextModule } from 'primeng/inputtext';
+import { TranslocoDirective } from '@jsverse/transloco';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    InputTextModule,
+    PasswordModule,
+    TranslocoDirective,
+  ],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.scss',
+})
+export class LoginFormComponent {
+  private readonly _formBuilder = inject(FormBuilder);
+
+  @Output() formChange: EventEmitter<any> = new EventEmitter<any>();
+
+  loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = this._formBuilder.group({
+      username: ['admin', [Validators.required]],
+      password: ['endurance', [Validators.required]],
+    });
+  }
+
+  handleSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const { username, password } = this.loginForm.value;
+
+    this.formChange.emit({ username, password });
+  }
+
+  checkErrors(field: string): string {
+    const form: any = this.loginForm.get(field);
+
+    if (form.invalid && (form.dirty || form.touched)) {
+      if (form?.hasError('required')) {
+        return 'Campo requerido';
+      }
+    }
+    return '';
+  }
+}
--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,66 +1,38 @@
 import { Component, inject, OnInit } from '@angular/core';
 
-import {
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  UntypedFormBuilder,
-  Validators,
-} from '@angular/forms';
-import { NgIf } from '@angular/common';
 import { Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 
-import { PasswordModule } from 'primeng/password';
-import { InputTextModule } from 'primeng/inputtext';
 import { MessageService } from 'primeng/api';
 import { Toast } from 'primeng/toast';
 
 import { AuthService } from 'app/core/auth/auth.service';
 import { UserService } from 'app/services';
 import { User } from 'app/domain';
+import { LoginFormComponent } from './login-form/login-form.component';
 
+import { TranslocoService } from '@jsverse/transloco';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, Toast, InputTextModule, PasswordModule],
+  imports: [Toast, LoginFormComponent],
   providers: [MessageService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
   private readonly _router = inject(Router);
-  private readonly _formBuilder = inject(FormBuilder);
 
   private readonly _unsubscribeAll: Subject<any> = new Subject<any>();
   private readonly _messageService = inject(MessageService);
+  private readonly _translocoService = inject(TranslocoService);
 
   private readonly _authService = inject(AuthService);
   private readonly _userService = inject(UserService);
 
-  loginForm: FormGroup;
-
-  constructor() {
-    this.loginForm = this._formBuilder.group({
-      username: ['admin', [Validators.required]],
-      password: ['endurance', [Validators.required]],
-    });
-  }
-
   ngOnInit(): void {}
 
-  handleSubmit(): void {
-    if (this.loginForm.invalid) {
-      this.loginForm.markAllAsTouched();
-      return;
-    }
-
-    const { username, password } = this.loginForm.value;
-
-    this.login({ username, password });
-  }
-
-  login(form: { username: string; password: string }) {
+  onFormChange(form: { username: string; password: string }) {
     this._authService
       .login(form)
       .pipe(takeUntil(this._unsubscribeAll))
@@ -69,13 +41,7 @@ export class LoginComponent implements OnInit {
           this.getUser();
         },
         error: (err) => {
-          this.loginForm.reset();
-          this._messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: 'Usuario o contraseña incorrectos',
-            life: 3000,
-          });
+          this.showToast(this.getTransloco('login.error'));
         },
       });
   }
@@ -84,12 +50,7 @@ export class LoginComponent implements OnInit {
     this._userService.profile().subscribe({
       next: (user: User) => {
         if (!user.is_superuser) {
-          this._messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: 'Usuario no autorizado',
-            life: 3000,
-          });
+          this.showToast(this.getTransloco('login.unauthorized'));
           return;
         }
 
@@ -101,15 +62,17 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  checkErrors(field: string): string {
-    const form: any = this.loginForm.get(field);
+  showToast(message: string) {
+    this._messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      icon: 'pi pi-exclamation-triangle',
+      detail: message,
+    });
+  }
 
-    if (form.invalid && (form.dirty || form.touched)) {
-      if (form?.hasError('required')) {
-        return 'Campo requerido';
-      }
-    }
-    return '';
+  getTransloco(key: string) {
+    return this._translocoService.translate(key);
   }
 
   ngOnDestroy(): void {
