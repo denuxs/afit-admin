b5232978b8bdb0654e5404c85d1f3c8b60da8f88
Denis López <denuxs@gmail.com>
2025-04-23 12:58:37 -0600

    filter exercise by muscle and equipment

--- src/app/pages/admin/exercises/exercises.component.ts
+++ src/app/pages/admin/exercises/exercises.component.ts
@@ -4,8 +4,8 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
 
-import { Exercise } from 'app/domain';
-import { ExerciseService } from 'app/services';
+import { Catalog, Exercise } from 'app/domain';
+import { CatalogService, ExerciseService } from 'app/services';
 
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
@@ -27,25 +27,48 @@ import { TooltipModule } from 'primeng/tooltip';
 export class ExercisesComponent implements OnInit {
   private readonly _formBuilder = inject(FormBuilder);
   private readonly _exerciseService = inject(ExerciseService);
+  private readonly _catalogService = inject(CatalogService);
 
   exercises$!: Observable<Exercise[]>;
+  catalogs$!: Observable<Catalog[]>;
 
   filterForm: FormGroup;
+
+  muscles: any = [];
+  equipments: any = [];
 
   constructor() {
     this.filterForm = this._formBuilder.group({
       search: ['', []],
+      muscle: ['', []],
+      equipment: ['', []],
     });
   }
 
   ngOnInit(): void {
-    this.getExercises();
+    this.getExercises({});
+    this.getCatalogs();
   }
 
-  getExercises(params?: { search?: string; key?: string }): void {
-    this.exercises$ = this._exerciseService.fetchExercises({
-      ...params,
-    });
+  getExercises(params?: any): void {
+    console.log(params);
+    params['ordering'] = '-id';
+    this.exercises$ = this._exerciseService.fetchExercises(params);
+  }
+
+  getCatalogs(): void {
+    this._catalogService
+      .fetchCatalogs({
+        ordering: '-id',
+      })
+      .subscribe({
+        next: (catalogs: Catalog[]) => {
+          this.muscles = catalogs.filter(catalog => catalog.key === 'muscle');
+          this.equipments = catalogs.filter(
+            catalog => catalog.key === 'equipment'
+          );
+        },
+      });
   }
 
   handleFilter() {
