14a4e6160968b4c5f059cbb6dca14f43e869430f
Denis López <denuxs@gmail.com>
2025-06-07 14:25:55 -0600

    add textarea component

--- src/app/components/prime-textarea/prime-textarea.component.ts
+++ src/app/components/prime-textarea/prime-textarea.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TextareaModule } from 'primeng/textarea';
 @Component({
   selector: 'app-prime-textarea',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, TextareaModule],
   templateUrl: './prime-textarea.component.html',
-  styleUrl: './prime-textarea.component.scss'
+  styleUrl: './prime-textarea.component.scss',
 })
 export class PrimeTextareaComponent {
-
+  @Input() form!: FormGroup;
+  @Input() controlName!: string;
+  @Input() label!: string;
+  @Input() placeholder = '';
+  @Input() help = '';
 }
